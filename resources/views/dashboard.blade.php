76ced63bc986744e9e83c16bf17090162d141f20
Agustín Durán <agustineduran@gmail.com>
2024-05-06 02:07:45 -0300

    feat: subir archivo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <form action="{{ url('/upload') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <label for="file" class="block text-gray-700 dark:text-gray-300 font-semibold mb-1">Subir Archivo:</label>
+                        <input type="file" name="file" id="file" accept=".pdf,.xls,.csv"
+                            class="p-6 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-900 dark:border-gray-700 dark:text-gray-200">
+                        <x-primary-button type="submit" class="mt-3 hover:bg-blue-600">
+                            Subir
+                        </x-primary-button>
+                        <!-- TODO: lang -->
+                    </form>
                 </div>
             </div>
         </div>
