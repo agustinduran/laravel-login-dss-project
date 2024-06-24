d393d3f9822bfdb8f5fe7bdcee2bac43bc11c912
Agustín Durán <agustineduran@gmail.com>
2024-06-24 00:29:05 -0300

    feat: Models

---
+++ app/Models/Student.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',  // Nombre
+        'last_name',   // Apellido
+    ];
+
+    public function grades()
+    {
+        return $this->hasMany(Grade::class);
+    }
+}
